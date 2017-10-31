0a2d6557ea9e8de12e1073e365dde43a31ab2604
suyapa alvarado <suyapa.caldex@hotmail.es>
2017-10-30 22:21:40 -0600

    main

--- src/advance/Advance.java
+++ src/advance/Advance.java
@@ -1,23 +1,36 @@
 package advance;
+import java.util.Scanner;
 
 public class Advance {
 
     public static void main(String[] args) {
         PersonManagement personManagement = new PersonManagement("./Personas.txt");
-
-        Person person = new Person("García", "Ilich", "Col. Loma Linda Norte", "Tegucigalpa", "Francisco Morazán", 504);
-        Person person1 = new Person("Mendoza", "Jerson", "Col. Venezuela", "Tegucigalpa", "Francisco Morazán", 504);
+        String lastname,firstname, address,city, dep;
+        Scanner input = new Scanner(System.in);
+        
+        System.out.println("Registro de Personas\n\n");
+        System.out.println("Ingrese el primer nombre: ");
+        firstname = input.next();
+        System.out.println("Ingrese el apellido: ");
+        lastname = input.next();
+        System.out.println("Ingrese la dirección: ");
+        address = input.next();
+        System.out.println("Ingrese la ciudad: ");
+        city = input.next();
+        System.out.println("Ingrese el departamento: ");
+        dep = input.next();
+        Person person = new Person(lastname, firstname, address, city, dep, 504);
 
         if (personManagement.getFile().exists()) { // Si el archivo existe, primero lo cargo.
             personManagement.loadFile(); // Pasa lo del ROM al ArrayList.
             
             personManagement.setPerson(person);
-            personManagement.setPerson(person1);
+
 
             personManagement.writeFile();
         } else { // Sino, lo creo y escribo en él.
             personManagement.setPerson(person);
-            personManagement.setPerson(person1);
+
 
             personManagement.writeFile();
         }
