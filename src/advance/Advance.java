796362304c5c436d450a6736ec67a9e18468cd56
ilich-garcia <ilich-garcia@unitec.edu>
2017-10-30 23:26:45 -0600

    Versión final

--- src/advance/Advance.java
+++ src/advance/Advance.java
@@ -1,36 +1,36 @@
 package advance;
+
 import java.util.Scanner;
 
 public class Advance {
 
     public static void main(String[] args) {
         PersonManagement personManagement = new PersonManagement("./Personas.txt");
-        String lastname,firstname, address,city, dep;
+        String lastname, firstname, address, city, state;
         Scanner input = new Scanner(System.in);
-        
+
         System.out.println("Registro de Personas\n\n");
-        System.out.println("Ingrese el primer nombre: ");
-        firstname = input.next();
-        System.out.println("Ingrese el apellido: ");
-        lastname = input.next();
-        System.out.println("Ingrese la dirección: ");
-        address = input.next();
-        System.out.println("Ingrese la ciudad: ");
-        city = input.next();
-        System.out.println("Ingrese el departamento: ");
-        dep = input.next();
-        Person person = new Person(lastname, firstname, address, city, dep, 504);
+        System.out.print("Ingrese el primer nombre: ");
+        firstname = input.nextLine();
+        System.out.print("Ingrese el apellido: ");
+        lastname = input.nextLine();
+        System.out.print("Ingrese la dirección: ");
+        address = input.nextLine();
+        System.out.print("Ingrese la ciudad: ");
+        city = input.nextLine();
+        System.out.print("Ingrese el departamento: ");
+        state = input.nextLine();
+        
+        Person person = new Person(lastname, firstname, address, city, state, 504);
 
         if (personManagement.getFile().exists()) { // Si el archivo existe, primero lo cargo.
             personManagement.loadFile(); // Pasa lo del ROM al ArrayList.
-            
-            personManagement.setPerson(person);
 
+            personManagement.setPerson(person);
 
             personManagement.writeFile();
         } else { // Sino, lo creo y escribo en él.
             personManagement.setPerson(person);
-
 
             personManagement.writeFile();
         }
--- src/advance/DelimTextBuffer.java
+++ src/advance/DelimTextBuffer.java
@@ -10,6 +10,7 @@ package advance;
  * @author JEAN WAHLUNG
  */
 public class DelimTextBuffer {
+
     char Delim = '|';
     int Maxbytes = 1000;
     char[] Buffer;
@@ -21,63 +22,75 @@ public class DelimTextBuffer {
         Delim = delim;
         DelimStr[0] = Delim;
         DelimStr[1] = 0;
+
         if (maxBytes < 0) {
             maxBytes = 0;
         }
+
         Maxbytes = maxBytes;
         Buffer = new char[Maxbytes];
         Buffersize = 0;
         return 1;
     }
-   
-    int Unpack(char str){
-        int len=-1;
+
+    int Unpack(char str) {
+        int len = -1;
         int start = NextByte;
+
         for (int i = start; i < Buffersize; i++) {
-            if (Buffer[i]==Delim) {
-                len=i-start;
+            if (Buffer[i] == Delim) {
+                len = i - start;
                 break;
             }
-            
+
         }
-        if(len ==-1){
+
+        if (len == -1) {
             return 1;
         }
-        NextByte+=len+1;
-        if(NextByte>Buffersize){
+
+        NextByte += len + 1;
+
+        if (NextByte > Buffersize) {
             return 1;
         }
-        char [] copia= new char[len+str];
+
+        char[] copia = new char[len + str];
         for (int i = 0; i < Buffer.length; i++) {
-            copia[i]=Buffer[i];
-            copia[i]= Buffer[str];
-            
+            copia[i] = Buffer[i];
+            copia[i] = Buffer[str];
+
         }
-        str=0;
-       
+
+        str = 0;
+
         return 0;
     }
-  
-    int Pack(char str, int size){
-        int len=0;
-        int lens=0;
-        if(size>=0)
-            len=size;
-        else
-            lens=str+len;
-        if(len>lens)
-            return 1;
-        int start=NextByte;
-        NextByte +=len+1;
-        if(NextByte>Maxbytes)
-            return 1;
-        System.arraycopy(Buffer, str, Buffer, len, Buffer.length);
-        Buffer[start+len]= Delim;
-        Buffersize=NextByte;
+
+    int Pack(char str, int size) {
+        int len = 0;
+        int lens = 0;
+        if (size >= 0) {
+            len = size;
+        } else {
+            lens = str + len;
+        }
         
-                    
+        if (len > lens) {
+            return 1;
+        }
+        
+        int start = NextByte;
+        NextByte += len + 1;
+        
+        if (NextByte > Maxbytes) {
+            return 1;
+        }
+
+        System.arraycopy(Buffer, str, Buffer, len, Buffer.length);
+        Buffer[start + len] = Delim;
+        Buffersize = NextByte;
+
         return 0;
     }
-
-
 }
